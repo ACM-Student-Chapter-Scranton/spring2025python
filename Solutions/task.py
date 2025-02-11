82b0af2bef99043083e9ef3dc2e38a8b42af4186
University of Scranton ACM Student Chapter <acm_student@scranton.edu>
2025-02-11 17:10:20 -0500

    Added Section on Objects

--- Solutions/task.py
+++ Solutions/task.py
@@ -1,26 +1,6 @@
-import csv
-import datetime
-
 class Task:
-    def __init__(self, name, desc, priority, deadline=datetime.datetime.max):
+    def __init__(self, name, desc, priority, is_done):
         self.name = name
         self.desc = desc
         self.priority = priority
-        self.is_done = False
-        self.time_created = datetime.datetime.now()
-        self.deadline = deadline
-    
-    def __str__(self):
-        return f'''{self.name}: Priority ({self.priority})\n{self.desc}\nDeadline: {self.deadline}\nCompleted: {self.is_done}'''
-    
-    def toggle_complete(self):
-        self.is_done = not self.is_done
-
-    def save_task(self, filename='saved_tasks.csv'):
-        with open(filename, "r+") as file:
-            rows = csv.reader(file)
-            fieldnames = next(rows)
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            data = [self.name, self.desc, self.priority, self.deadline, self.is_done]
-            entry = dict(zip(fieldnames, data))
-            writer.writerow(entry)
+        self.is_done = is_done
--- Solutions/todo.py
+++ Solutions/todo.py
@@ -1,88 +1,25 @@
 import csv
-import pandas
-import datetime
-
 from task import Task
 
-def create_task():
-    name = input("Task Name: ")
-    desc = input("Task Desc: ")
-    priority = input("Task Priority: ")
+first = Task('First Task', 'This is the first task I am creating as a part of this program', 1, False)
+second = Task('Second Task', 'A second task to complete after my first one.', 1, False)
+third = Task('Third Task', 'This task has a higher priority than the other two', 3, False)
+fourth = Task('Fourth Task', 'This task has already been completed because it\'s priority was so high', 10, True)
 
-    deadline_choice = input('Set Deadline [y/n]? ')
-    date = None
+tasks =[first, second, third, fourth]
 
-    if deadline_choice == 'y':
-        year = int(input('Year: '))
-        month = int(input('Month: '))
-        day = int(input('Day: '))
-        date = datetime.datetime(year, month, day)
+for task in tasks:
+    print(task.name)
 
-    if date:
-        new_task = Task(name, desc, priority, date)
-    else:
-        new_task = Task(name, desc, priority)
+new_task = Task('New Task', 'Added using a Python program that opens files', 2, False)
 
-    print("Creation Success!")
-    print(new_task)
+with open('saved_tasks.txt', 'a') as file:
+    entry = f'{new_task.name},{new_task.desc},{new_task.priority},{new_task.is_done}\n'
+    file.write(entry)
 
-    save_task = input("Save Task [y/n]?")
-
-    if save_task == 'y':
-        new_task.save_task()
-        print("Saved!")
-
-def list_tasks(select=None):
-    with open('saved_tasks.csv', 'r') as file:
-        file.seek(0)
-        rows = csv.reader(file)
-        headers = next(rows)
-
-        if select:
-            indices = [headers.index(colname) for colname in select]
-            headers = select
-        else:
-            indices = []
-
-        print("Tasks")
-        print('--------------------------------')
-
-        for line_no, line in enumerate(rows, start=1):
-
-            if indices:
-                line = [ line[index] for index in indices ]
-
-            print(line_no, line)
-            print('--------------------------------')
-
-def modify_tasks():
-    list_tasks(select=['name', 'is_done'])
-    choice = int(input('Choose Task to Modify: ')) - 1
-
-    df = pandas.read_csv('saved_tasks.csv')
-    df.loc[choice, 'is_done'] = not df.loc[choice, 'is_done']
-    df.to_csv('saved_tasks.csv', index=False)    
-
-def main():
-    print('Hello, User!')
-    choice = ''
-    while choice != '0':
-        print('Main Menu (type number)')
-        print('0. Exit')
-        print('1. Create New Task')
-        print('2. Show Saved Tasks')
-        print('3. Modify Task Status')
-        choice = input('>>>>> ')
-        if choice == '1':
-            create_task()
-        elif choice == '2':
-            list_tasks()
-        elif choice == '3':
-            modify_tasks()
-        elif choice != '0':
-            print('Invalid choice, please type a single digit.')
-
-    print('Goodbye!')
-
-if __name__ == '__main__':
-    main()
+with open('saved_tasks.csv', 'a') as csvfile:
+    fieldnames = ['name', 'desc', 'priority', 'is_done']
+    fieldvalues = [new_task.name, new_task.desc, new_task.priority, new_task.is_done]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    entry = dict(zip(fieldnames, fieldvalues))
+    writer.writerow(entry)
